7b60bc70f67f81527db9859acc8b04617bd03f3c
hkmr <harshkmr991234@gmail.com>
2021-05-08 22:56:41 +0530

    project for placement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Col,
+  Form,
+  FormGroup,
+  Row,
+} from "react-bootstrap";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>ToDo App</h1>
+        <Form onSubmit={this.addItem}>
+          <Row className="align-items-center">
+            <Col>
+              <FormGroup>
+                <Form.Control
+                  ref={(a) => (this._inputElement = a)}
+                  type="name"
+                  placeholder="Enter Item name"
+                  required
+                ></Form.Control>
+              </FormGroup>
+            </Col>
+            <Col>
+              <Button type="submit" variant="success">
+                ADD
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        {this.state.items.map((item) => {
+          return <h2 key={item.id}>{item.name}</h2>;
+        })}
+      </div>
+    );
+  }
+
+  addItem = (e) => {
+    e.preventDefault();
+
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        name: this._inputElement.value,
+        id: Date.now(),
+      };
+
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem),
+        };
+      });
+
+      this._inputElement.value = "";
+    }
+  };
 }
-
-export default App;
